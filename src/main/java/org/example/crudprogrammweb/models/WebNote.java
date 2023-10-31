c2e4e7683f353be9c6620337766fdc452499522b
namsyr <namsyr.sharapov@gmail.com>
2023-10-31 19:42:41 +0300

    Initial commit

---
+++ src/main/java/org/example/crudprogrammweb/models/WebNote.java
@@ -0,0 +1,30 @@
+package org.example.crudprogrammweb.models;
+
+public class WebNote {
+    private int id;
+    private String note;
+
+    public WebNote (int id, String note) {
+        this.id = id;
+        this.note = note;
+    }
+
+    public  WebNote() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
